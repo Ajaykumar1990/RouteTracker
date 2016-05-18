b59840e677df72ae2eda2ebf22c31f5fea07e34f
Ajay Kumar <ajay905016@gmail.com>
2016-05-18 03:25:26 -0700

    Initial Commit

---
+++ app/src/main/java/tracker/route/com/routetracker/MainActivity.java
@@ -0,0 +1,18 @@
+package tracker.route.com.routetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/**
+ * creted by Ajay Kumar test
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
